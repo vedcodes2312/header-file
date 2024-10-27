1556f8be507d3510e68f3c2628b2495907a5b437
Ved Vyas <vedvyascodes1608@gmail.com>
2024-10-27 21:24:18 +0530

    Add files via upload

---
+++ MY_OWN_HEADER_FILE/myheader.h
@@ -0,0 +1,15 @@
+/* a simple code to demonstrate creating one's own header file
+AUTHOR: VED VYAS     GITHUB:vedcodes2312        */
+
+
+#ifndef MYHEADER_H  
+#define MYHEADER.H   
+
+void greetings(void);
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+int modulo(int a, int b);
+
+#endif 
